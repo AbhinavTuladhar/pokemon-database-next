f20ddf5e7edc5b912fe2993ce9f2deadec6e4681
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-02-13 13:53:48 +0545

    feat: make types of common utilities

---
+++ src/types/utils/Language.d.ts
@@ -0,0 +1,11 @@
+import { Resource } from "./Resource";
+import { Name } from "./Common";
+
+export interface Language {
+  id: number,
+  iso3166: string,
+  iso639: string,
+  name: string,
+  names: Array<Name>,
+  official: boolean
+}
---
+++ src/types/utils/NamedResource.d.ts
@@ -0,0 +1,11 @@
+export interface NamedApiResource<T> {
+  name: string,
+  url: string
+}
+
+export interface NamedApiResourceList<T> {
+  count: number,
+  next: string,
+  previous: string,
+  results: Array<NamedApiResource<T>>
+}
