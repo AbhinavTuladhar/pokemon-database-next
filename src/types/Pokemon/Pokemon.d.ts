5c248e6b5f0ea8e3e7bc9001fdec84e1fec3b135
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-02-15 13:39:06 +0545

    feat: make interfaces for Pokemon sprites

--- src/types/Pokemon/Pokemon.d.ts
+++ src/types/Pokemon/Pokemon.d.ts
@@ -31,7 +31,7 @@ export interface Pokemon {
   past_abilities: Array<TODO>
   past_types: Array<PokemonPastType>
   species: NamedApiResource<PokemonSpecies>
-  sprites: TODO
+  sprites: PokemonSprites
   stats: PokemonStat
   types: Array<PokemonType>
   weight: number
@@ -64,6 +64,46 @@ export interface PokemonPastType {
   types: Array<PokemonType>
 }
 
+export interface PokemonSprites {
+  back_default: string
+  back_female: string
+  back_shiny: string
+  back_shiny_female: string
+  front_default: string
+  front_female: string
+  front_shiny: string
+  front_shiny_female: string
+  other: PokemonSpritesOther
+  versions: PokemonSpritesVersion
+}
+
+export interface PokemonSpritesVariant {
+  back_default: string | null
+  back_female: string | null
+  back_shiny: string | null
+  back_gray: string | null
+  back_shiny_female: string | null
+  back_transparent: string | null
+  back_shiny_transparent: string | null
+  front_default: string | null
+  front_female: string | null
+  front_shiny: string | null
+  front_gray: string | null
+  front_shiny_female: string | null
+  front_transparent: string | null
+  front_shiny_transparent: string | null
+}
+
+export interface PokemonSpritesOther {
+  dream_world: Pick<PokemonSpritesVariant, 'front_default' | 'front_female'>
+  home: Pick<
+    PokemonSpritesVariant,
+    'front_default' | 'front_female' | 'front_shiny' | 'front_shiny_female'
+  >
+  'official-artwork': Pick<PokemonSpritesVariant, 'front_default' | 'front_shiny'>
+  showdown: Omit<PokemonSpritesVariant, 'back_gray' | 'front_gray'>
+}
+
 export interface PokemonStat {
   base_stat: number
   effort: number
@@ -88,4 +128,131 @@ export interface PokemonHeldItem {
 export interface PokemonHeldItemVersion {
   rarity: number
   version: NamedApiResource<Version>
+}
+
+/**
+ * --------------------------------------------------
+ * --------------ALL SPRITE INTERFACES---------------
+ * --------------------------------------------------
+ */
+
+export interface PokemonVersionSprites {
+  'generation-i': Generation1Sprites
+}
+export interface Generation1Sprites {
+  'red-blue': Pick<
+    PokemonSpritesVariant,
+    | 'back_default'
+    | 'back_gray'
+    | 'front_default'
+    | 'front_gray'
+    | 'back_transparent'
+    | 'front_transparent'
+  >
+  yellow: Pick<
+    PokemonSpritesVariant,
+    | 'back_default'
+    | 'back_gray'
+    | 'front_default'
+    | 'front_gray'
+    | 'back_transparent'
+    | 'front_transparent'
+  >
+}
+
+export interface Generation2Sprites {
+  crystal: Pick<
+    PokemonSpritesVariant,
+    | 'back_default'
+    | 'back_shiny'
+    | 'back_shiny_transparent'
+    | 'back_transparent'
+    | 'front_default'
+    | 'front_shiny'
+    | 'front_shiny_transparent'
+    | 'front_transparent'
+  >
+  gold: Pick<
+    PokemonSpritesVariant,
+    'back_default' | 'back_shiny' | 'front_default' | 'front_shiny' | 'front_transparent'
+  >
+  silver: Pick<
+    PokemonSpritesVariant,
+    'back_default' | 'back_shiny' | 'front_default' | 'front_shiny' | 'front_transparent'
+  >
+}
+
+export interface Generation3Sprites {
+  emerald: Pick<PokemonSpritesVariant, 'front_default' | 'front_shiny'>
+  'firered-leafgreen': Pick<
+    PokemonSpritesVariant,
+    'back_default' | 'back_shiny' | 'front_default' | 'front_shiny'
+  >
+  'ruby-sapphire': Pick<
+    PokemonSpritesVariant,
+    'back_default' | 'back_shiny' | 'front_default' | 'front_shiny'
+  >
+}
+
+export interface Generation4Sprites {
+  'diamond-pearl': Omit<PokemonSpritesVariant, 'back_shiny_transparent' | 'front_shiny_transparent'>
+  'heartgold-soulsilver': Omit<
+    PokemonSpritesVariant,
+    'back_shiny_transparent' | 'front_shiny_transparent'
+  >
+  platinum: Omit<PokemonSpritesVariant, 'back_shiny_transparent' | 'front_shiny_transparent'>
+}
+
+export interface Generation5Sprites {
+  'black-white': {
+    animated: Omit<PokemonSpritesVariant, 'back_shiny_transparent' | 'front_shiny_transparent'>
+    back_default: string
+    back_female: string
+    back_shiny: string
+    back_shiny_female: string
+    front_default: string
+    front_female: string
+    front_shiny: string
+    front_shiny_female: string
+  }
+}
+
+export interface Generation6Sprites {
+  'omegaruby-alphasapphire': Pick<
+    PokemonSpritesVariant,
+    'front_default' | 'front_female' | 'front_shiny' | 'front_shiny_female'
+  >
+  'x-y': Pick<
+    PokemonSpritesVariant,
+    'front_default' | 'front_female' | 'front_shiny' | 'front_shiny_female'
+  >
+}
+
+export interface Generation7Sprites {
+  icons: {
+    front_default: string | null
+    front_female: string | null
+  }
+  'ultra-sun-ultra-moon': Pick<
+    PokemonSpritesVariant,
+    'front_default' | 'front_female' | 'front_shiny' | 'front_shiny_female'
+  >
+}
+
+export interface Generation8Sprites {
+  icons: {
+    front_default: string | null
+    front_female: string | null
+  }
+}
+
+export interface PokemonSpritesVersion {
+  'generation-i': Generation1Sprites
+  'generation-ii': Generation2Sprites
+  'generation-iii': Generation3Sprites
+  'generation-iv': Generation4Sprites
+  'generation-v': Generation5Sprites
+  'generation-vi': Generation6Sprites
+  'generation-vii': Generation7Sprites
+  'generation-viii': Generation8Sprites
 }
