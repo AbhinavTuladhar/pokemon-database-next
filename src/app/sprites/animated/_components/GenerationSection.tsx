5b6cfd956ab75cfbe5fdd7ee2603383b5ee00635
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-03-20 19:22:30 +0545

    feat: make generation-wise sprite tables

---
+++ src/app/sprites/animated/_components/GenerationSection.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react'
+
+import generationData from '@/data/generationData'
+import type { NamedApiResource, Pokemon } from '@/types'
+
+import SpriteTable from './SpriteTable'
+
+interface SectionProps {
+  generationNumber: number
+  pokemonData: Array<NamedApiResource<Pokemon>>
+}
+
+const GenerationSection: FC<SectionProps> = ({ generationNumber, pokemonData }) => {
+  return (
+    <div className="space-y-4 border-b-2 border-gray-300 pb-8">
+      <h2 className="text-4xl font-bold"> Generation {generationNumber} </h2>
+      <div className="grid grid-cols-sprite-table gap-10">
+        {pokemonData.map((row, index) => {
+          // Get the id number for the gif
+          const idNumber = parseInt(row.url.match(/\/(\d+)\/$/)![1])
+          return <SpriteTable id={idNumber} pokemonName={row.name} key={index} />
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default GenerationSection
--- src/app/sprites/animated/page.tsx
+++ src/app/sprites/animated/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
+import generationData from '@/data/generationData'
 import { PokemonApi } from '@/services/PokemonApi'
 
+import GenerationSection from './_components/GenerationSection'
 import SpriteTable from './_components/SpriteTable'
 
 const getPokemonList = async () => {
@@ -13,16 +15,29 @@ const getPokemonList = async () => {
 const AnimatedSprites = async () => {
   const data = await getPokemonList()
 
+  const generations = Array.from({ length: 7 }, (_, index) => index + 1)
+
   // Process some of the data
 
   return (
-    <main>
-      <h1 className="my-4 text-center text-5xl font-bold">Animated Pokémon Sprite Collection</h1>
-      <div className="grid-cols-sprite-table grid gap-10">
-        {data.map((row, index) => (
-          <SpriteTable id={index + 1} pokemonName={row.name} key={index} />
-        ))}
-      </div>
+    <main className="space-y-4">
+      <h1 className="mt-4 text-center text-5xl font-bold">Animated Pokémon Sprite Collection</h1>
+      <p>The table represents the regular and shiny sprites in generation 7, respectively. </p>
+      {generations.map((generationNumber) => {
+        const genData = generationData[generationNumber - 1]
+
+        const startIndex = genData.offset
+        const endIndex = startIndex + genData.limit
+        const generationPokemonData = data.slice(startIndex, endIndex)
+
+        return (
+          <GenerationSection
+            key={generationNumber}
+            pokemonData={generationPokemonData}
+            generationNumber={generationNumber}
+          />
+        )
+      })}
     </main>
   )
 }
