c6b7da333d82a843e6f111098dd879018de99200
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-04-15 19:28:42 +0545

    add filtering to move table

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,7 +3,7 @@ import { FC, InputHTMLAttributes } from 'react'
 const Input: FC<InputHTMLAttributes<HTMLInputElement>> = ({ ...props }) => {
   return (
     <input
-      className="w-64 max-w-full rounded-lg px-2 py-2 text-black placeholder-gray-300"
+      className="w-64 max-w-full rounded-lg px-2 py-2 text-black placeholder-gray-500"
       {...props}
     />
   )
