8f007db4d709f4db7cb97310dcbfb303a3019e55
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-05-01 11:31:39 +0545

    change navbar hover colour

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -19,7 +19,7 @@ interface TopLevelProps {
 }
 const DropDownItem: FC<Path> = ({ path, name }) => {
   return (
-    <li className="block bg-gray-900 px-4 py-1.5 text-left text-white duration-500 hover:brightness-125">
+    <li className="block bg-gray-900 px-4 py-1.5 text-left text-white duration-500 hover:bg-gray-700">
       <Link href={path} className="flex w-full">
         <span>{name}</span>
       </Link>
