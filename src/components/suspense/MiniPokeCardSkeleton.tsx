f435c17824255cfb6977fb34e0430f9fa7dee578
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-05-03 21:22:11 +0545

    change case of suspense components

---
+++ src/components/suspense/MiniPokeCardSkeleton.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import Skeleton from 'react-loading-skeleton'
+
+export const MiniPokeCardSkeleton = () => {
+  return <Skeleton containerClassName="flex-1 w-full" className="h-20 w-full" />
+}
---
+++ src/components/suspense/TypeSummarySkeleton.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react'
+import Skeleton from 'react-loading-skeleton'
+
+interface CardProps {
+  count: number
+}
+
+export const TypeSummarySkeleton: FC<CardProps> = ({ count }) => {
+  return (
+    <div className="flex flex-row flex-wrap items-center justify-center gap-8">
+      {Array(count)
+        .fill(0)
+        .map((_, index) => (
+          <div className="h-[52px] w-[123px]" key={index}>
+            <Skeleton key={index} containerClassName="flex-1 w-full" className="h-full" />
+          </div>
+        ))}
+    </div>
+  )
+}
