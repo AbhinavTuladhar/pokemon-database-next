3f035906b6b88d9c0094779a3d4392833e68be95
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-05-05 18:29:02 +0545

    make temp type chart w/o first row

--- src/app/dual-type-chart/_components/DualTypeChart.tsx
+++ src/app/dual-type-chart/_components/DualTypeChart.tsx
@@ -1,21 +1,23 @@
 import React, { FC } from 'react'
 
-import { TypeCard } from '@/components/cards'
+import { MiniTypeCard, TypeCard, TypeMultiplierBox } from '@/components/cards'
 import typeList from '@/data/typeList'
+import { TransformedType } from '@/types'
+import calculateOffensiveTypeEffectiveness from '@/utils/typeEffectivenessOffensive'
 
 interface DualTypeChartProps {
   baseType: string
+  attackingTypeInfo: Array<Pick<TransformedType, 'doubleDamageTo' | 'halfDamageTo' | 'noDamageTo'>>
 }
 
-const DualTypeChart: FC<DualTypeChartProps> = ({ baseType }) => {
+const DualTypeChart: FC<DualTypeChartProps> = ({ baseType, attackingTypeInfo }) => {
   // Sorting like this brings the identical array ('fire', 'fire') to the top.
-  const typeCombinations = typeList
-    .map(type => [baseType, type])
-    .sort((a, b) => {
-      if (a[0] === a[1]) return -1
-      if (b[0] === b[1]) return 1
-      return 0
-    })
+  const typeCombinations = typeList.map(type => [baseType, type])
+  // .sort((a, b) => {
+  //   if (a[0] === a[1]) return -1
+  //   if (b[0] === b[1]) return 1
+  //   return 0
+  // })
 
   const cornerDiv = (
     <div className="-mb-px flex h-[36px] flex-col items-center justify-center rounded border border-table-border text-[10px]">
@@ -24,22 +26,46 @@ const DualTypeChart: FC<DualTypeChartProps> = ({ baseType }) => {
     </div>
   )
 
-  const firstColumn = (
-    <div className="flex w-min flex-col gap-y-px">
-      <>{cornerDiv}</>
-      {typeCombinations.map((type, index) => {
-        const [firstType, secondType] = type
-        return (
-          <div className="flex gap-x-px" key={index}>
-            <TypeCard key={index} typeName={firstType} />
-            {firstType !== secondType ? <TypeCard key={index} typeName={secondType} /> : null}
-          </div>
+  const firstColumnCards = typeCombinations.map((type, index) => {
+    const [firstType, secondType] = type
+    return (
+      <div className="flex gap-x-px" key={index}>
+        <TypeCard key={index} typeName={firstType} variant="big" />
+        {firstType !== secondType ? (
+          <TypeCard key={index} typeName={secondType} variant="big" />
+        ) : null}
+      </div>
+    )
+  })
+
+  const tableColumns = [...typeList].map((type, colIndex) => {
+    const colDiv = typeCombinations.map((typeComb, rowIndex) => {
+      if (rowIndex === 0) {
+        return <MiniTypeCard typeName={type} key={rowIndex} />
+      } else {
+        const multiplierVaue = calculateOffensiveTypeEffectiveness(
+          typeComb,
+          attackingTypeInfo[colIndex],
         )
-      })}
+        return <TypeMultiplierBox multiplier={multiplierVaue} key={rowIndex} />
+      }
+    })
+    return (
+      <div className="flex flex-col" key={colIndex}>
+        {colDiv}
+      </div>
+    )
+  })
+
+  return (
+    <div className="flex gap-x-px">
+      <div className="flex flex-col gap-y-px">
+        {cornerDiv}
+        {firstColumnCards}
+      </div>
+      <div className="flex flex-row gap-x-px">{tableColumns}</div>
     </div>
   )
-
-  return <div>{firstColumn}</div>
 }
 
 export default DualTypeChart
--- src/app/dual-type-chart/page.tsx
+++ src/app/dual-type-chart/page.tsx
@@ -1,31 +1,24 @@
 import React from 'react'
 
-import { TypeCard } from '@/components/cards'
 import { PageTitle } from '@/components/containers'
 import typeList from '@/data/typeList'
 import TypeExtractor from '@/extractors/TypeExtractor'
 import { TypesApi } from '@/services'
-import findTypeEffectiveness from '@/utils/findTypeEffectiveness'
 
 import DualTypeChart from './_components/DualTypeChart'
 
 const getAllTypeData = async () => {
   const typeData = await TypesApi.getByNames(typeList)
 
-  // Step 1: Extract type information
-  // Step 2: Calculate the type chart, and return an object containing the type chart with the defending
-  // type name
-  // Step 3: Properly format the type chart object.
-  // Next we need to transform the data into a usable state.
   const transformedTypeData = typeData.map(type => {
     const extractedInfo = TypeExtractor(type)
-    const { name: typeName } = extractedInfo
-    const typeChart = findTypeEffectiveness([extractedInfo])
-    const typeDefenceInfo = Object.entries(typeChart).map(([typeName, multiplier]) => ({
-      typeName,
-      multiplier,
-    }))
-    return { typeName, typeDefenceInfo }
+    const { name: typeName, doubleDamageTo, halfDamageTo, noDamageTo } = extractedInfo
+    const attackingTypeInfo = {
+      doubleDamageTo,
+      halfDamageTo,
+      noDamageTo,
+    }
+    return { typeName, attackingTypeInfo }
   })
 
   return transformedTypeData
@@ -34,40 +27,15 @@ const getAllTypeData = async () => {
 const DualTypePage = async () => {
   const typeData = await getAllTypeData()
 
-  const cornerDiv = (
-    <div className="-mb-px flex h-[36px] flex-col items-center justify-center rounded border border-[#292e38] text-[10px]">
-      <span> ATTACK → </span>
-      <span> DEFENCE ↴ </span>
-    </div>
-  )
-
-  // Find all the possible type combinations
-  const typeCombinations = typeList.flatMap(type => {
-    return typeList.map(innerType => {
-      return [type, innerType]
-    })
-  })
-
-  const firstColumn = (
-    <div className="flex w-min flex-col gap-y-px">
-      <>{cornerDiv}</>
-      {typeCombinations.map((type, index) => {
-        const [firstType, secondType] = type
-        return (
-          <div className="flex gap-x-px" key={index}>
-            <TypeCard key={index} typeName={firstType} />
-            <TypeCard key={index} typeName={secondType} />
-          </div>
-        )
-      })}
-    </div>
-  )
-
   return (
     <main>
       <PageTitle> Pokémon dual-type charts </PageTitle>
-      {typeList.map(type => (
-        <DualTypeChart key={type} baseType={type} />
+      {typeList.map((type, index) => (
+        <DualTypeChart
+          key={type}
+          baseType={type}
+          attackingTypeInfo={typeData.map(data => data.attackingTypeInfo)}
+        />
       ))}
     </main>
   )
