a599c23f284a6392a74271f8a3d3b8f50046b545
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-05-05 18:46:10 +0545

    make proper typecharts

--- src/app/dual-type-chart/_components/DualTypeChart.tsx
+++ src/app/dual-type-chart/_components/DualTypeChart.tsx
@@ -12,12 +12,13 @@ interface DualTypeChartProps {
 
 const DualTypeChart: FC<DualTypeChartProps> = ({ baseType, attackingTypeInfo }) => {
   // Sorting like this brings the identical array ('fire', 'fire') to the top.
-  const typeCombinations = typeList.map(type => [baseType, type])
-  // .sort((a, b) => {
-  //   if (a[0] === a[1]) return -1
-  //   if (b[0] === b[1]) return 1
-  //   return 0
-  // })
+  const typeCombinations = typeList
+    .map(type => [baseType, type])
+    .sort((a, b) => {
+      if (a[0] === a[1]) return -1
+      if (b[0] === b[1]) return 1
+      return 0
+    })
 
   const cornerDiv = (
     <div className="-mb-px flex h-[36px] flex-col items-center justify-center rounded border border-table-border text-[10px]">
@@ -38,24 +39,30 @@ const DualTypeChart: FC<DualTypeChartProps> = ({ baseType, attackingTypeInfo }) 
     )
   })
 
-  const tableColumns = [...typeList].map((type, colIndex) => {
-    const colDiv = typeCombinations.map((typeComb, rowIndex) => {
-      if (rowIndex === 0) {
-        return <MiniTypeCard typeName={type} key={rowIndex} />
-      } else {
-        const multiplierVaue = calculateOffensiveTypeEffectiveness(
-          typeComb,
-          attackingTypeInfo[colIndex],
-        )
-        return <TypeMultiplierBox multiplier={multiplierVaue} key={rowIndex} />
-      }
+  const firstRow = typeList.map((type, index) => <MiniTypeCard typeName={type} key={index} />)
+
+  const otherRows = typeCombinations.map((typeCombo, rowIndex) => {
+    const rowDiv = typeList.map((_, colIndex) => {
+      const [firstType, secondType] = typeCombo
+      const multiplierValue = calculateOffensiveTypeEffectiveness(
+        firstType === secondType ? [firstType] : typeCombo,
+        attackingTypeInfo[colIndex],
+      )
+      return <TypeMultiplierBox multiplier={multiplierValue} key={colIndex} />
     })
     return (
-      <div className="flex flex-col" key={colIndex}>
-        {colDiv}
+      <div className="flex" key={rowIndex}>
+        {rowDiv}
       </div>
     )
   })
+
+  const talbeRows = (
+    <div className="flex flex-col">
+      <div className="flex gap-x-px">{firstRow}</div>
+      {otherRows}
+    </div>
+  )
 
   return (
     <div className="flex gap-x-px">
@@ -63,7 +70,7 @@ const DualTypeChart: FC<DualTypeChartProps> = ({ baseType, attackingTypeInfo }) 
         {cornerDiv}
         {firstColumnCards}
       </div>
-      <div className="flex flex-row gap-x-px">{tableColumns}</div>
+      <div className="flex flex-col gap-y-px">{talbeRows}</div>
     </div>
   )
 }
