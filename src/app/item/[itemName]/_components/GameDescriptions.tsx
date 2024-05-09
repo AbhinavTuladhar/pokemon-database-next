acdaece650027d02be2641e7c8d579d968890b05
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-05-09 11:55:07 +0545

    use component in item description

--- src/app/item/[itemName]/_components/GameDescriptions.tsx
+++ src/app/item/[itemName]/_components/GameDescriptions.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import { FC } from 'react'
 
-import {
-  SectionTitle,
-  TableCell,
-  TableCellHeader,
-  TableContainer,
-  TableRow,
-} from '@/components/containers'
+import { SectionTitle } from '@/components/containers'
+import GameWiseDescriptions from '@/components/game-wise-descriptions'
 import { gameBlackLists } from '@/data/blacklists'
-import gameNameMap from '@/data/gameNameMap'
 import { TransformedItem } from '@/types'
 
 type DescriptionInterface = Pick<TransformedItem, 'descriptions'>
@@ -21,15 +15,15 @@ interface ItemByGeneration {
   [key: string]: {
     description: string
     generation: string
-    versionName: Array<string>
+    versionGroupNames: Array<string>
   }
 }
 
 const groupData = (descriptions: DescriptionInterface['descriptions']) => {
   return descriptions
-    .filter(description => !gameBlackLists.includes(description.versionName))
+    .filter(description => !gameBlackLists.includes(description.versionGroupName))
     .reduce((acc, curr) => {
-      const { description: rawDescription, generation, versionName } = curr
+      const { description: rawDescription, generation, versionGroupName } = curr
       // There are escape characters in the descriptions, which shall now be removed.
       const description = rawDescription.replace(/\n/g, ' ')
 
@@ -38,10 +32,10 @@ const groupData = (descriptions: DescriptionInterface['descriptions']) => {
         acc[generation] = {
           description,
           generation,
-          versionName: [versionName],
+          versionGroupNames: [versionGroupName],
         }
       } else {
-        acc[generation].versionName.push(versionName)
+        acc[generation].versionGroupNames.push(versionGroupName)
       }
       return acc
     }, {} as ItemByGeneration)
@@ -53,28 +47,10 @@ export const GameDescriptions: FC<TableProps> = ({ descriptions }) => {
   // Filter out undefined generations
   const properGroupedData = Object.values(groupedData).filter(row => row.generation !== undefined)
 
-  const tableRows = properGroupedData.map((generationKey, rowIndex) => {
-    const { description, versionName } = generationKey
-    return (
-      <TableRow key={rowIndex}>
-        <TableCellHeader wrapFlag={true}>
-          <ul>
-            {versionName.map((version, index) => (
-              <li key={index}> {gameNameMap[version]} </li>
-            ))}
-          </ul>
-        </TableCellHeader>
-        <TableCell>{description}</TableCell>
-      </TableRow>
-    )
-  })
-
   return (
     <>
       <SectionTitle>Game Descriptions</SectionTitle>
-      <TableContainer>
-        <tbody>{tableRows}</tbody>
-      </TableContainer>
+      <GameWiseDescriptions descriptionData={properGroupedData} />
     </>
   )
 }
