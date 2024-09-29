ca4a0fb01dd57fd84234bfec9d29ecb2b0575076
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-09-29 17:36:55 +0545

    feat: style main move page

--- src/app/move/page.tsx
+++ src/app/move/page.tsx
@@ -1,22 +1,61 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { Metadata } from 'next'
-import Link from 'next/link'
+
+import { PageTitle, SectionTitle } from '@/components/containers'
+import BlueLink from '@/components/link'
+import { MOVE_LIST_TEXT } from '@/data/hardCodedText'
 
 export const metadata: Metadata = {
   title: 'Pokémon move list | Pokémon Database',
 }
 
-const MoveList = async () => {
+const SideBar: FC = () => {
   const numbers = Array.from({ length: 7 }, (_, index) => index + 1)
 
   return (
-    <div>
-      {numbers.map(number => (
-        <Link href={`/move/generation/${number}`} key={number}>
-          Generation {number}
-        </Link>
+    <aside className="self-start rounded bg-sky-100 px-4 pb-4 pt-px dark:bg-table-header">
+      <SectionTitle> Moves by Generation </SectionTitle>
+      <ul className="list-inside list-disc space-y-2">
+        {numbers.map(number => (
+          <li key={number}>
+            <BlueLink href={`/move/generation/${number}`} key={number}>
+              Generation {number}
+            </BlueLink>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  )
+}
+
+const MainDescription: FC = () => {
+  const paragraphs = MOVE_LIST_TEXT.split('\n')
+
+  return (
+    <>
+      <h2 className="text-4xl font-bold"> About Pokémon Moves </h2>
+      {paragraphs.map((paragraph, index) => (
+        <div key={index}>
+          {/* Capitalse the first letter of each paragraph. */}
+          {paragraph.charAt(0).toUpperCase() + paragraph.slice(1)}
+          {index !== paragraphs?.length - 1 && <br />}
+        </div>
       ))}
-    </div>
+    </>
+  )
+}
+
+const MoveList = async () => {
+  return (
+    <main>
+      <PageTitle> Pokémon Moves</PageTitle>
+      <div className="grid gap-x-8 lg:grid-cols-3">
+        <SideBar />
+        <div className="lg:col-span-2">
+          <MainDescription />
+        </div>
+      </div>
+    </main>
   )
 }
 
