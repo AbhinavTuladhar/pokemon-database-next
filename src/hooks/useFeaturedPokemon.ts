a9b7d3ba6fd5e7b57a3a17ea9b0f4cd3adf3ac59
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-10-14 16:14:28 +0545

    feat: make featured pokemon in homepage dynamic

---
+++ src/hooks/useFeaturedPokemon.ts
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+
+import generationData from '@/data/generationData'
+import PokemonExtractor from '@/extractors/PokemonExtractor'
+import { PokemonApi } from '@/services'
+import { TransformedPokemon } from '@/types'
+
+const useFeaturedPokemon = () => {
+  const [pokemonData, setPokemonData] = useState<TransformedPokemon[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState<any>(null)
+
+  const getRandomNumber = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  useEffect(() => {
+    const getRandomPokemonData = async () => {
+      // Generate four random numbers between 1 and 809 for the Pokemon ids, one for each generation.
+      const generationIds = generationData.map(generation => {
+        const { limit, offset } = generation
+        const lowerLimit = offset
+        const upperLimit = offset + limit
+        return getRandomNumber(lowerLimit, upperLimit)
+      })
+      const responses = await PokemonApi.getByIds(generationIds)
+      const extractedData = responses.map(PokemonExtractor)
+      setPokemonData(extractedData)
+    }
+
+    try {
+      setIsLoading(true)
+      getRandomPokemonData()
+    } catch (error) {
+      setError(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }, [])
+
+  return { pokemonData, isLoading, error }
+}
+
+export default useFeaturedPokemon
