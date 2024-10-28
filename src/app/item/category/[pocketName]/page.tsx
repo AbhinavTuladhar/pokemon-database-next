ef29b8643ea1f139b01f80ebf8f0ef1c7d11e5a4
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-10-28 17:28:05 +0545

    feat: pre-render item pages

--- src/app/item/category/[pocketName]/page.tsx
+++ src/app/item/category/[pocketName]/page.tsx
@@ -3,17 +3,27 @@ import { Metadata } from 'next'
 
 import { PageTitle } from '@/components/containers'
 import { LoadingPageFallback } from '@/components/skeletons'
+import { ItemApi } from '@/services'
 
 import { ItemTableWrapper } from './_components'
+
+export const metadata: Metadata = {
+  title: 'List of Pokémon Items | Pokémon Database',
+}
+const getPocketNames = async () => {
+  const response = await ItemApi.getAllItemPockets()
+  return response.sort((a, b) => (a > b ? 1 : -1))
+}
+
+export async function generateStaticParams() {
+  const pocketNames = await getPocketNames()
+  return pocketNames.map(name => ({ pocketName: name }))
+}
 
 interface PageProps {
   params: {
     pocketName: string
   }
-}
-
-export const metadata: Metadata = {
-  title: 'List of Pokémon Items | Pokémon Database',
 }
 
 const ItemPage: FC<PageProps> = async ({ params: { pocketName } }) => {
