a16968f35ec1769b9c13a2e4c6ce0de8b4e0d2ae
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-10-29 09:29:57 +0545

    feat: add route visualiser page

---
+++ src/app/visualise/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import Visualizer from 'next-route-visualizer'
+
+const VisualisePage = () => <Visualizer baseURL="http://localhost:5173" />
+
+export default VisualisePage
