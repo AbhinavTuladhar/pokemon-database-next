77ec5dd4fb71b8196d666d87b24fe16e7b6bc959
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-12-01 09:45:45 +0545

    feat: use different navbar animation on mobile

--- src/components/navbar/top-level-menu.tsx
+++ src/components/navbar/top-level-menu.tsx
@@ -2,7 +2,9 @@
 
 import { FC, ReactNode, useState } from 'react'
 import classNames from 'classnames'
-import { AnimatePresence, motion } from 'framer-motion'
+import { AnimatePresence, HTMLMotionProps, motion } from 'framer-motion'
+
+import { useMediaQuery } from '@/hooks/useMediaQuery'
 
 import DropDownItem from './drop-down-item'
 
@@ -20,10 +22,39 @@ interface TopLevelProps {
 
 const TopLevelMenu: FC<TopLevelProps> = ({ menuData, mobileText, icon, desktopText }) => {
   const [isOpen, setIsOpen] = useState(false)
+  const isMobile = useMediaQuery('(max-width: 768px)')
 
   const closeMenu = () => setIsOpen(false)
   const openMenu = () => setIsOpen(true)
   const toggleMenu = () => setIsOpen(!isOpen)
+
+  // transition just opacity on mobile, transition opacity and height on desktop
+  const dynamicProps: HTMLMotionProps<'ul'> = {
+    ...(isMobile
+      ? {
+          initial: { opacity: 0 },
+          animate: {
+            opacity: 1,
+            transition: {
+              delay: 0.3,
+              ease: 'easeInOut',
+            },
+          },
+          exit: { opacity: 0 },
+        }
+      : {
+          initial: { opacity: 0, clipPath: 'inset(0 0 100% 0)' },
+          animate: {
+            opacity: 1,
+            clipPath: 'inset(0 0 0 0)',
+            transition: {
+              delay: 0.15,
+            },
+          },
+          exit: { opacity: 0, clipPath: 'inset(0 0 100% 0)' },
+        }),
+    transition: { duration: 0.4, ease: 'easeInOut' },
+  }
 
   return (
     <li
@@ -50,10 +81,7 @@ const TopLevelMenu: FC<TopLevelProps> = ({ menuData, mobileText, icon, desktopTe
       <AnimatePresence>
         {isOpen && (
           <motion.ul
-            initial={{ opacity: 0, clipPath: 'inset(0 0 100% 0)' }}
-            animate={{ opacity: 1, clipPath: 'inset(0 0 0 0)' }}
-            exit={{ opacity: 0, clipPath: 'inset(0 0 100% 0)' }}
-            transition={{ duration: 0.4, ease: 'easeOut' }}
+            {...dynamicProps}
             className={classNames(
               'absolute left-0 right-0 top-16 z-50 text-center text-white transition-opacity duration-500 md:top-14',
             )}
