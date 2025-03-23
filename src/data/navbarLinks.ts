0908e9089718de5b0f6a7c58d599386ca1364b0b
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2025-03-23 09:35:36 +0545

    feat: remove competitive page

--- src/data/navbarLinks.ts
+++ src/data/navbarLinks.ts
@@ -15,7 +15,6 @@ export const pokemonDataLinks = [
   { path: '/sprites', name: 'Sprite gallery' },
   { path: '/sprites/animated', name: 'Animated sprites' },
   { path: '/location', name: 'Locations' },
-  { path: '/competitive', name: 'Competitive' },
 ]
 
 export const mechanicsLinks = [
